e327b1d253a283e250b53517645c0b85a0c77b1f
Nattiwut Yotnoi <nattiwut.yotnoi@gmail.com>
2023-03-18 00:25:31 +0700

    upProject.net

---
+++ BlazorCalculatorApp/Pages/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorCalculatorApp.Pages
+{
+    public class Calc
+    { 
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public string symbol { get; set; }
+        public int total { get; set; }
+    }
+}
